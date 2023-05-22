ddcaebb2835a4b1de5e0c6594d56f1f7a53ac6f2
M Abdullah Sani <mabdullahsani@Ms-MacBook-Pro.local>
2023-05-22 13:39:16 +0800

    udpate

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -8,7 +8,7 @@ export default function App({ Component, pageProps }: AppProps) {
     <>
       <Head>
         <title>HIT Digital Indonesia</title>
-        <link rel="icon" href="./favicon.ico" />
+        <link rel="icon" href="/favicon.ico" />
       </Head>
       <Component {...pageProps} />
     </>
